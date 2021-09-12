fe116b983bfae0d4430c68f7a1bf9f2c9176482f
LENGUE <damico.lengue@sodecoton.cm>
2021-09-12 19:46:28 +0100

    Ajout de Swagger pour la documentation de l'API Documentation personalisée du controller ActiviteNettoyageD

--- GpaoRest/src/main/java/com/sdcc/gpao/GpaoRestApplication.java
+++ GpaoRest/src/main/java/com/sdcc/gpao/GpaoRestApplication.java
@@ -5,8 +5,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+
 
 @SpringBootApplication
+@EnableSwagger2
 public class GpaoRestApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
@@ -17,5 +31,22 @@ public class GpaoRestApplication extends SpringBootServletInitializer {
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 	  return builder.sources(GpaoRestApplication.class);
 	}
-
+	
+	@Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.sdcc.gpao"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+	
+	private ApiInfo apiInfo() {
+		 return new ApiInfoBuilder().title("Documentation de l'API REST de la GPAO")
+		 .description("API REST pour gérer les services back-end de la GPAO")
+		 .contact(new Contact("LENGUE Damico", "SODECOTON", "damico.lengue@sodecoton.cm"))
+		 .version("1.0")
+		 .build();
+	}
+    
 }
--- GpaoRest/src/main/java/com/sdcc/gpao/controller/ActiviteNettoyageDController.java
+++ GpaoRest/src/main/java/com/sdcc/gpao/controller/ActiviteNettoyageDController.java
@@ -17,40 +17,62 @@ import com.sdcc.gpao.entity.ActiviteNettoyageD;
 import com.sdcc.gpao.exception.ResourceNotFoundException;
 import com.sdcc.gpao.service.ActiviteNettoyageDService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
 @RequestMapping("/api/activitenettoyaged/")
+@Api(value = "REST controlleur pour les activités de l'atelier nettoyage décorticage")
 public class ActiviteNettoyageDController {
 
 	@Autowired
 	private ActiviteNettoyageDService activiteNettoyageDService;
 	
+	@ApiOperation(value = "Liste toutes les données sur la saisie des activités de l'atelier nettoyage décorticage", response = Collection.class)
+	@ApiResponses(value = {@ApiResponse(code = 302, message = "La liste des activités est retournée, ou une liste vide")})
 	@GetMapping("/liste")
 	public ResponseEntity<Collection<ActiviteNettoyageD>> getListe(){
 		return activiteNettoyageDService.getListe();
 	}
 	
+	@ApiOperation(value = "L'objet ayant pour ID parmi les données sur les activités de l'atelier nettoyage décorticage", response = ActiviteNettoyageD.class)
+	@ApiResponses(value = {@ApiResponse(code = 302, message = "Les données de l'activités ont été retournées"),
+			@ApiResponse(code = 404, message = "L'activité ayant l'ID demandé n'existe pas")})
 	@GetMapping("/liste/{id}")
 	public ResponseEntity<ActiviteNettoyageD> getElement(@PathVariable(value = "id") int id) throws ResourceNotFoundException{
 		return activiteNettoyageDService.getElement(id);
 	}
 	
 	@GetMapping("/production/{annee}/{mois}/{jour}")
+	@ApiOperation(value = "Retourne les données sur les activités de l'atelier nettoyage décorticage pour une date précise", response = Collection.class)
+	@ApiResponses(value = {@ApiResponse(code = 302, message = "Les données de l'activités ont été retournées"),
+			@ApiResponse(code = 404, message = "Les données de l'activités demandé n'existent pas")})
 	public ResponseEntity<Collection<ActiviteNettoyageD>> getActivite(@PathVariable(value = "annee") int annee, @PathVariable(value = "mois") int mois,
 			@PathVariable(value = "jour") int jour) throws ResourceNotFoundException{
 		return activiteNettoyageDService.getDonnneeActivite(annee, mois, jour);
 	}
 	
 	@PostMapping("/ajouter")
+	@ApiOperation(value = "Ajoute une occurence des activités de l'atelier nettoyage décorticage", response = ActiviteNettoyageD.class)
+	@ApiResponses(value = {@ApiResponse(code = 201, message = "Les données de l'activités ont été ajouté avec succès")})
 	public ResponseEntity<ActiviteNettoyageD> ajouter(@RequestBody ActiviteNettoyageD t){
 		return activiteNettoyageDService.sauvegarder(t);
 	}
 	
 	@PutMapping("/modifier")
+	@ApiOperation(value = "Modifie une occurence des activités de l'atelier nettoyage décorticage", response = ActiviteNettoyageD.class)
+	@ApiResponses(value = {@ApiResponse(code = 200, message = "Les données de l'activités ont été modifié avec succès"),
+			@ApiResponse(code = 404, message = "L'activité ayant l'ID demandé n'existe pas")})
 	public ResponseEntity<ActiviteNettoyageD> modifier(@RequestBody ActiviteNettoyageD t) throws ResourceNotFoundException{
 		return activiteNettoyageDService.modifier(t);
 	}
 	
 	@DeleteMapping("/supprimer")
+	@ApiOperation(value = "Supprime une occurence des activités de l'atelier nettoyage décorticage", response = ActiviteNettoyageD.class)
+	@ApiResponses(value = {@ApiResponse(code = 200, message = "Les données de l'activités ont été supprimé avec succès"),
+			@ApiResponse(code = 404, message = "L'activité ayant l'ID demandé n'existe pas")})
 	public void supprimer(@RequestBody ActiviteNettoyageD t) throws ResourceNotFoundException{
 		activiteNettoyageDService.supprimer(t);
 	}
